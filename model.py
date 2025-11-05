6ac221e3ff742b50a11ca1993215456aa96fadf4
akaz9579 <akaz9579@gmail.com>
2025-11-05 10:59:46 -0500

    split data fix

--- model.py
+++ model.py
@@ -4,19 +4,24 @@ import os
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import AdaBoostClassifier, RandomForestRegressor, GradientBoostingRegressor
+import xgboost as xgb
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, classification_report
+import matplotlib.pyplot as plt
 
 
 
 #Import data
-filePath = "/home/ahk23009/Law/data"
+filePath = "/home/ahk23009/Law/data/PaperData"
 os.chdir(filePath)
-csvFile = "motionStrike_TVcodes_data.tsv"
-dataSet = pd.read_csv(csvFile)
-print("Data:\n", dataSet.head())
+trainData = "train.csv"
 
+trainSet = pd.read_csv(trainData)
+print("Data:\n", trainSet.head())
+
+testData = 'test.csv'
+testSet = pd.read_csv(testData)
 
 #Filtering out our data:
 
