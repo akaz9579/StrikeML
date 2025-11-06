4e3ba640bda3e7761f7e1fc37b44bff78c858c4c
akaz9579 <akaz9579@gmail.com>
2025-11-06 00:54:28 -0500

    entropy

--- model.py
+++ model.py
@@ -1,37 +1,56 @@
 import pandas as pd
 import numpy as np
 import os
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import GridSearchCV
 from sklearn.preprocessing import LabelEncoder, StandardScaler
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.ensemble import AdaBoostClassifier, RandomForestRegressor, GradientBoostingRegressor
+from sklearn.tree import DecisionTreeClassifier 
 import xgboost as xgb
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import accuracy_score, classification_report, r2_score
 import matplotlib.pyplot as plt
 
 
 
-#Import data
+#Import data, set up data
 filePath = "/home/ahk23009/Law/data/PaperData"
 os.chdir(filePath)
+
+#training data set up 
 trainData = "train.csv"
-
 trainSet = pd.read_csv(trainData)
-print("Data:\n", trainSet.head())
+trainSet = trainSet[["CaseLocation", "CaseMajorCode", "MotionJurisNumber", "MotionResultCode"]]
 
-testData = 'test.csv'
-testSet = pd.read_csv(testData)
 
-#Filtering out our data:
+#---------------------compute entropy for training---------------------
+caseCounts = trainSet.groupby(["MotionJurisNumber", "CaseMajorCode"]).size().unstack(fill_value=0)
+alpha = 1
+smoothed = caseCounts + alpha
+prob = smoothed.div(smoothed.sum(axis=1), axis=0)
 
-trainSet.dropna(subset=[], inplace=True)
-x_train = None 
+entropy = -np.sum(prob * np.log(prob), axis=1)
+trainSet = trainSet.merge(entropy.rename("AttorneyEntropy"), left_on="MotionJurisNumber", right_index=True)
+print(trainSet.head())
+#---------------------compute entropy for training---------------------
+
+x_train = trainSet["CaseLocation", "CaseMajorCode", "MotionJurisNumber","AttorneyEntropy"]
 y_train = trainSet["MotionResultCode"] # binary 1 or 0, 1 is granted 0 is denyed 
 
-x_test =  None 
-y_test = None
+#test data set up 
+testData = 'test.csv'
+testSet = pd.read_csv(testData)
+testSet = testSet["CaseLocation", "CaseMajorCode", "MotionJurisNumber", "MotionResultCode"]
 
+#entropy:
+caseCountsT = testSet.groupby(["MotionJurisNumber", "CaseMajorCode"]).size().unstack(fill_value=0)
+smoothedT = caseCountsT + alpha
+probT = smoothedT.div(smoothedT.sum(axis=1), axis=0)
+entropyT = -np.sum(probT * np.log(probT), axis=1)
+testSet = testSet.merge(entropyT.rename("AttorneyEntropy"), left_on="MotionJurisNumber", right_index=True)
+print(testSet.head())
+
+
+x_test =  None 
+y_test = testSet["MotionResultCode"]
 
 
 # Testing our Models: 
@@ -43,7 +62,7 @@ rForest= RandomForestRegressor()
 gBoost = GradientBoostingRegressor()
 
 models = [ada,dTree,rForest, gBoost]
-predict= 
+#predict= 
 for model in models:
     model.fit()
 
